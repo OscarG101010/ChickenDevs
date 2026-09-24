e3f6f2d6de720c56c2a9f013ec78fc4c4cba94e3
oscar <al45314@un.edu.mx>
2026-09-24 03:30:27 -0600

    agrega lógica de carrito para total a pagar

--- Equipo4/Equipo4/Models/Carrito.cs
+++ Equipo4/Equipo4/Models/Carrito.cs
@@ -3,7 +3,11 @@
     public class Carrito
     {
         public List<ItemCarrito> Items { get; set; } = new List<ItemCarrito>();
-        public decimal Total => Items.Sum(i => i.Subtotal);
+
+        public const decimal TasaIva = 0.16m;
+        public decimal GranSubtotal => Items.Sum(i => i.Subtotal);
+        public decimal Iva => Math.Round(GranSubtotal * TasaIva, 2);
+        public decimal Total => GranSubtotal + Iva;
 
         public void Agregar(Producto p)
         {
